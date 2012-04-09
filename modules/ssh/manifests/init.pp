cbb2d053444eeeb3c2f1b0ba8e9e6fd70266f58a
Peng Yong <ppyy@pubyun.com>
2012-04-10 02:32:25 +0800

    1. ssh 的缺省改成 53399 端口，减少扫描 2. 只有 ssh 组的用户，才能登录

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -1,15 +1,25 @@
 class ssh {
-    package { 'openssh-server': ensure => latest }
-    file { '/etc/ssh/sshd_config':
-      ensure => 'present',
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0444',
-      source => [
-          'puppet:///modules/ssh/sshd_config.$operatingsystem',
-          'puppet:///modules/ssh/sshd_config'
-      ],
-      replace => 'true',
+    package {
+        'openssh-client': ensure => latest;
+        'openssh-server': ensure => latest;
+    }
+    file { 
+      '/etc/ssh/ssh_config':
+          source  => [ 'puppet:///modules/ssh/ssh_config' ],
+          require => Package['openssh-client']
+        ; 
+      '/etc/ssh/sshd_config':
+          ensure => 'present',
+          owner  => 'root',
+          group  => 'root',
+          mode   => '0444',
+          source => [
+              'puppet:///modules/ssh/sshd_config.$operatingsystem',
+              'puppet:///modules/ssh/sshd_config'
+          ],
+          replace => 'true',
+          require => Package['openssh-client']
+        ;
     }
     service { 'ssh':
       ensure          => running,
